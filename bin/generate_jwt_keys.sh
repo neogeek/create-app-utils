90a498adac67c9f6d71957478a9d3fd31add0cc4
Scott Doxey <neogeek0101@gmail.com>
2023-01-06 14:24:22 -0500

    Changed temp file name.

--- bin/generate_jwt_keys.sh
+++ bin/generate_jwt_keys.sh
@@ -10,20 +10,20 @@ generate() {
 
     ENV_PREFIX=$1
 
-    openssl genrsa -out rsa_2048_priv.pem -passout pass:"${PASSPHRASE}" 2048 &>/dev/null
+    openssl genrsa -out rsa_priv.pem -passout pass:"${PASSPHRASE}" 2048 &>/dev/null
 
-    openssl rsa -pubout -in rsa_2048_priv.pem -out rsa_2048_pub.pem &>/dev/null
+    openssl rsa -pubout -in rsa_priv.pem -out rsa_pub.pem &>/dev/null
 
     if [[ "${OSTYPE}" == "darwin"* ]]; then
-        PRIVATE_KEY=$(base64 <rsa_2048_priv.pem)
-        PUBLIC_KEY=$(base64 <rsa_2048_pub.pem)
+        PRIVATE_KEY=$(base64 <rsa_priv.pem)
+        PUBLIC_KEY=$(base64 <rsa_pub.pem)
     else
-        PRIVATE_KEY=$(base64 -w 0 <rsa_2048_priv.pem)
-        PUBLIC_KEY=$(base64 -w 0 <rsa_2048_pub.pem)
+        PRIVATE_KEY=$(base64 -w 0 <rsa_priv.pem)
+        PUBLIC_KEY=$(base64 -w 0 <rsa_pub.pem)
     fi
 
-    rm -f rsa_2048_priv.pem
-    rm -f rsa_2048_pub.pem
+    rm -f rsa_priv.pem
+    rm -f rsa_pub.pem
 
     if ! test -f "../.env"; then
         cp ../.env.development ../.env
